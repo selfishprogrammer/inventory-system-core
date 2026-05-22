d9f1d479e50e1ce7802ce6891db4c28c7a9be594
DESKTOP-A8IN8LUser <rahuljha3246@gmail.com>
2026-05-22 15:09:01 +0530

    feature: Added Inventory System

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 connectDB();
 initSocket(server);
 
-app.use(cors({ origin: process.env.CLIENT_URL ?? 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin:'*', credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
